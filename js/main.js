d277fac11ebfd67c42626c36d8f2a16fd1afb3c3
dragonleolin <leo@stantex.com.tw>
2019-04-26 11:27:35 +0800

    cameraOutput

--- js/main.js
+++ js/main.js
@@ -210,7 +210,7 @@ function takeSnapshot() {
     context.drawImage(video, 0, 0, width, height);
 
     //take a picture 
-    cameraOutput.src = cameraSensor.toDataURL("image/jpeg");
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 
     // polyfil if needed https://github.com/blueimp/JavaScript-Canvas-to-Blob
