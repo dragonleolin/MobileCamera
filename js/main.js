d0d70b6106876056069f4e5996894aa99d3d4541
dragonleolin <leo@stantex.com.tw>
2019-05-13 11:46:14 +0800

    img

--- js/main.js
+++ js/main.js
@@ -304,7 +304,7 @@ function takeSnapshot() {
 }
 
 function uploadFile() {
-  alert('previewImg');
+  alert('Img');
   const canvas = document.getElementById("camera--sensor");
 
   const cameraOutput = document.querySelector("#camera--output");
